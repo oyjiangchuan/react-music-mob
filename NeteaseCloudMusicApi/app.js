dc3bca7052585512f28ca633c6930d3e11aca9ce
ouyjc <1366325462@qq.com>
2020-09-08 23:06:33 +0800

    default port to 4000

--- NeteaseCloudMusicApi/app.js
+++ NeteaseCloudMusicApi/app.js
@@ -90,7 +90,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const host = process.env.HOST || ''
 
 app.server = app.listen(port, host, () => {
